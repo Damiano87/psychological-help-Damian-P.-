aa98dc85693cb4ff52c595a486ce4ff86f164ce5
dampiwowarczyk <dampiwowarczyk@gmail.com>
2026-07-29 20:54:55 +0200

    Update page.tsx

--- app/offer/konsultacje-psychologiczne/page.tsx
+++ app/offer/konsultacje-psychologiczne/page.tsx
@@ -52,8 +52,7 @@ const PsychologicalConsultations = () => {
               <p className="text-neutral-700 text-lg leading-relaxed">
                 Konsultacja psychologiczna to spotkanie, podczas którego
                 zrozumiesz swoje wyzwania i kontekst sytuacyjny wraz z afektami
-                i emocjami im towarzyszącymi. Wspólnie wypracujemy rozwiązanie
-                oraz konkretny plan działania.
+                im towarzyszącymi.
               </p>
 
               {/* <p className="text-neutral-700 text-lg leading-relaxed">
